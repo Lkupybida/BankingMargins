4de6a86f7783696e7bd1775a6535e80094803956
Lev <87573704+Lkupybida@users.noreply.github.com>
2024-04-26 19:16:53 +0300

    added missing data for pivdennyi and some graphs

--- transpose.py
+++ transpose.py
@@ -3,15 +3,14 @@ import os
 
 def transpose_resample(name):
     file_name = name
-    name = 'original_data/' + name
+    name = 'pivdenny/' + name
     df = pd.read_csv(name)
     df = df.transpose()
     df.columns = df.iloc[0]
     df = df[1:]
     df.index = pd.to_datetime(df.index, format='%Y-%m-%d').strftime('%m/%Y')
-    new_name = 'improved_data/' + file_name
+    new_name = 'pivdenniy/' + file_name
     df.to_csv(new_name)
-
 
 # for name in ['AE.csv', 'C.csv', 'C.csv', 'D.csv', 'L.csv', 'LA.csv', 'NI.csv', 'NII.csv', 'TA.csv', 'TI.csv']:
 #     transpose_resample(name)
@@ -29,8 +28,8 @@ def select_banks(banks_list, file):
     df.to_csv(output_file)
 
 banks_list = ['cb privatbank', 'credit agricole bank', 'fuib', 'kredobank', 'oschadbank', 'otp bank', 'pivdennyi bank', 'raiffeisen bank', 'sense bank', 'ukrsibbank', 'universal bank']
-for file in ['AE.csv', 'C.csv', 'C.csv', 'D.csv', 'L.csv', 'LA.csv', 'NI.csv', 'NII.csv', 'TA.csv', 'TI.csv', 'NPL.csv', 'CL.csv']:
-    select_banks(banks_list, file)
+# for file in ['AE.csv', 'C.csv', 'C.csv', 'D.csv', 'L.csv', 'LA.csv', 'NI.csv', 'NII.csv', 'TA.csv', 'TI.csv', 'NPL.csv', 'CL.csv']:
+#     select_banks(banks_list, file)
 
 # for name in ['NPL.csv']:
 #     select_banks(banks_list, name)
