d13f535de6a0d4253a7bf077720a287a2b1c9b50
Lev <87573704+Lkupybida@users.noreply.github.com>
2024-04-28 15:04:38 +0300

    started creating variables, added TE

--- transpose.py
+++ transpose.py
@@ -3,20 +3,20 @@ import os
 
 def transpose_resample(name):
     file_name = name
-    name = 'pivdenny/' + name
+    name = 'original_data/' + name
     df = pd.read_csv(name)
     df = df.transpose()
     df.columns = df.iloc[0]
     df = df[1:]
     df.index = pd.to_datetime(df.index, format='%Y-%m-%d').strftime('%m/%Y')
-    new_name = 'pivdenniy/' + file_name
+    new_name = 'improved_data/' + file_name
     df.to_csv(new_name)
 
 # for name in ['AE.csv', 'C.csv', 'C.csv', 'D.csv', 'L.csv', 'LA.csv', 'NI.csv', 'NII.csv', 'TA.csv', 'TI.csv']:
 #     transpose_resample(name)
 
-# for name in ['NPL.csv']:
-#     transpose_resample(name)
+for name in ['TE.csv']:
+    transpose_resample(name)
 
 def select_banks(banks_list, file):
     file_name, file_extension = os.path.splitext(file)
@@ -31,5 +31,19 @@ banks_list = ['cb privatbank', 'credit agricole bank', 'fuib', 'kredobank', 'osc
 # for file in ['AE.csv', 'C.csv', 'C.csv', 'D.csv', 'L.csv', 'LA.csv', 'NI.csv', 'NII.csv', 'TA.csv', 'TI.csv', 'NPL.csv', 'CL.csv']:
 #     select_banks(banks_list, file)
 
-# for name in ['NPL.csv']:
-#     select_banks(banks_list, name)
+for name in ['TE.csv']:
+    select_banks(banks_list, name)
+
+def divide(file1, file2):
+    df1 = pd.read_csv(file1)
+    df2 = pd.read_csv(file2)
+
+    for i in range(1, len(df1)):
+        for j in range(1, len(df1.columns)):
+            df1.iloc[i, j] /= df2.iloc[i, j]
+
+    return df1
+
+#
+# output_file = 'only_sophomores/CR.csv'
+# divide('selected_banks/NPL_sel.csv', 'selected_banks/CL_sel.csv').to_csv(output_file)
